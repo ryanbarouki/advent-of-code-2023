2c99a5a54ed6200e007e43d98aee26ac1bb494a4
Ryan Barouki <ryanbarouki@gmail.com>
2023-12-13 16:42:36 +0000

    day 12 finallyyyyy

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,48 +1,54 @@
-def binary_strings(n, s=""):
-    if n == 0:
-        yield s
-    else:
-        yield from binary_strings(n-1,s+'#')
-        yield from binary_strings(n-1,s+'.')
+from collections import deque
+from functools import cache
 
-def find_runs(s,char):
-    counts = []
-    count = 1
-    for i, c in enumerate(s):
-        if i < len(s) - 1:
-            next_c = s[i+1]
-            if c != char:
-                continue
-            if c == next_c:
-                count += 1
-            else:
-                counts.append(count)
-                count = 1
-        else:
-            if c == char:
-                counts.append(count)
-    return tuple(counts)
+@cache
+def dp(seq, groups):
+    # base cases
+    if not seq:
+        # no more sequence left so add 1 if groups are also done, else 0
+        return groups == ()
+    if not groups:
+        # no more groups so return 1 if the rest of seq is '.', else 0
+        return '#' not in seq
+
+    # recursive time (one day dynamic programming will be natural I hope)
+    # also what even is DP?? - someone explain pls
+    count = 0
+    if seq[0] in ['.','?']:
+        # we go again if c = . or ?
+        count += dp(seq[1:],groups) 
+    if seq[0] in ['#','?']:
+        ns, ng = len(seq), groups[0]
+        # is the length of remaining sequence enough to fit the group?
+        # if we have a '.' in the section up to group length then it's not
+        # gonna work so do nothing
+        if ns >= ng and '.' not in seq[:ng]:
+            # if the remaining sequence length == group length 
+            # => continue recursion
+            # or if the next character in the sequence after the end of
+            # the group is a '.' or a '?'. A '#' would extend the group so we 
+            # recursing in that case.
+            if ng == ns or seq[ng] != '#':
+                # continue skipping the group in the seq
+                # and pop it from groups
+                count += dp(seq[ng+1:], groups[1:])
+    return count
 
 with open('input.txt') as f:
-    total = 0
+    total_part1 = 0
+    total_part2 = 0
     for line in f.readlines():
         s, counts = line.strip().split()
         counts = tuple([int(n) for n in counts.split(',')])
+        count1 = dp(s, counts)
+        total_part1 += count1
 
-        count_qm = s.count('?')
-        for bs in binary_strings(count_qm):
-            new_s = ''
-            index = 0
-            for c in s:
-                if c != '?':
-                    new_s += c
-                else:
-                    new_s += bs[index]
-                    index += 1
-            curr_counts = find_runs(new_s, '#')
-            if counts == curr_counts:
-                total += 1
-    print(total)
-            
+        # part 2
+        s_part2 = "?".join([s for i in range(5)])
+        counts_part2 = 5*counts
+        count2 = dp(s_part2, counts_part2)
+        total_part2 += count2
 
+    print(f"Part 1: {total_part1}")
+    print(f"Part 2: {total_part2}")
 
